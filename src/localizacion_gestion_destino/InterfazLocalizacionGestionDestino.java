34e3afa7cfb3becfe8bdd9d99f9f8284dbad6ca9
ismasaga <ismael.saborido@outlook.es>
2018-04-21 15:34:37 +0200

    Feita a interfaz de localización e destino e creadas as clases dependentes

--- src/localizacion_gestion_destino/InterfazLocalizacionGestionDestino.java
+++ src/localizacion_gestion_destino/InterfazLocalizacionGestionDestino.java
@@ -1,5 +1,41 @@
 package localizacion_gestion_destino;
 
-public interface InterfazLocalizacionGestionDestino {
+import java.util.ArrayList;
 
+import main.Destino;
+import main.Fecha;
+import main.Tiempo;
+
+public interface InterfazLocalizacionGestionDestino {
+	public void anadirDestino(Destino d);
+
+	public Fecha buscarFecha(Destino d);
+
+	public Tiempo calcularTiempoDetenido();
+
+	public Tiempo calcularTiempoMovimiento();
+
+	public Integer calcularNumeroParadas();
+
+	public Float calcularNumeroKilometros();
+
+	public Float calcularMediaVelocidad();
+
+	public Float calcularMediaParadas();
+
+	public Tiempo calcularMediaTiempoParada();
+
+	public Tiempo calcularMediaTiempoEntrega();
+
+	public Tiempo calcularMediaTiempoRecogida();
+
+	public ArrayList<Tiempo> getTiemposDesplazamiento();
+
+	public ArrayList<Tiempo> getTiemposDesplazamientosAcum();
+
+	public Float getPorcentajeTiempoParadas();
+
+	public Float getPorcentajeTiempoMovimiento();
+
+	public Float getPorcentajeTiempoEntregas();
 }
