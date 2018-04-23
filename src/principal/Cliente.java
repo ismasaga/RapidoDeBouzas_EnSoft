3ea1f94895cf50668285533fc80c8d4b39625175
ismasaga <ismael.saborido@outlook.es>
2018-04-23 21:13:49 +0200

    Implementadas as clases Fecha, Cliente e Paquete

--- src/principal/Cliente.java
+++ src/principal/Cliente.java
@@ -1,5 +1,46 @@
 package principal;
 
 public class Cliente {
-
+	private String dni;
+	private String nombre;
+	private Destino direccion;
+	private String telefono;
+	private boolean firma;
+	public Cliente(String dni, String nombre, Destino direccion, String telefono, boolean firma) {
+		this.dni = dni;
+		this.nombre = nombre;
+		this.direccion = direccion;
+		this.telefono = telefono;
+		this.firma = firma;
+	}
+	public String getDni() {
+		return dni;
+	}
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public Destino getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(Destino direccion) {
+		this.direccion = direccion;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	public boolean isFirma() {
+		return firma;
+	}
+	public void setFirma(boolean firma) {
+		this.firma = firma;
+	}
 }
--- src/principal/Paquete.java
+++ src/principal/Paquete.java
@@ -1,5 +1,78 @@
 package principal;
 
 public class Paquete {
+	private String id;
+	private Destino d;
+	private String observaciones;
+	private String dniAlternativo;
+	private String nombreAlternativo;
+	private boolean firmaAlternativa;
+	private EstadoPaquete estado;
+	
+	public Paquete(String id, Destino d, String observaciones, String dniAlternativo, String nombreAlternativo,
+			boolean firmaAlternativa, EstadoPaquete estado) {
+		this.id = id;
+		this.d = d;
+		this.observaciones = observaciones;
+		this.dniAlternativo = dniAlternativo;
+		this.nombreAlternativo = nombreAlternativo;
+		this.firmaAlternativa = firmaAlternativa;
+		this.estado = estado;
+	}
 
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public Destino getD() {
+		return d;
+	}
+
+	public void setD(Destino d) {
+		this.d = d;
+	}
+
+	public String getObservaciones() {
+		return observaciones;
+	}
+
+	public void setObservaciones(String observaciones) {
+		this.observaciones = observaciones;
+	}
+
+	public String getDniAlternativo() {
+		return dniAlternativo;
+	}
+
+	public void setDniAlternativo(String dniAlternativo) {
+		this.dniAlternativo = dniAlternativo;
+	}
+
+	public String getNombreAlternativo() {
+		return nombreAlternativo;
+	}
+
+	public void setNombreAlternativo(String nombreAlternativo) {
+		this.nombreAlternativo = nombreAlternativo;
+	}
+
+	public boolean isFirmaAlternativa() {
+		return firmaAlternativa;
+	}
+
+	public void setFirmaAlternativa(boolean firmaAlternativa) {
+		this.firmaAlternativa = firmaAlternativa;
+	}
+
+	public EstadoPaquete getEstado() {
+		return estado;
+	}
+
+	public void setEstado(EstadoPaquete estado) {
+		this.estado = estado;
+	}
 }
