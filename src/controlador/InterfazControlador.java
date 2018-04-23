b0d127a0875e1d57b6c1df4ac503d7cd08aa3ce0
ismasaga <ismael.saborido@outlook.es>
2018-04-24 00:16:23 +0200

    Comezando co módulo de controlador e feita a clase Tiempo

--- src/controlador/InterfazControlador.java
+++ src/controlador/InterfazControlador.java
@@ -4,15 +4,23 @@ import java.util.ArrayList;
 import principal.*;
 
 public interface InterfazControlador {
-	// comenteinas para que non dese fallo
-//	public Paquete solicitarPaquete(String id);
-//	public void anadirPaquete(Paquete p);
-//	public void borrarPaquete(String id);
-//	public Paquete buscarPaquete(String id);
-//	public void modificarPaquete(Paquete p);
+	public Paquete solicitarPaquete(String id);
+
+	public void anadirPaquete(Paquete p);
+
+	public void borrarPaquete(String id);
+
+	public Paquete buscarPaquete(String id);
+
+	public void modificarPaquete(Paquete p);
+
 	public void notificarDetencion(Camion c); // ou esta ou notificarDestino penso
-//	public void enviarCamion(Camion c);
-//	public ArrayList<Paquete> solicitarListaPaquetes();
-//	public Tiempo solicitarTiempoEstimadoEntregaPaquete(Paquete p);
-//    public void notificarDestino(Destino d);
+
+	public void enviarCamion(Camion c);
+
+	public ArrayList<Paquete> solicitarListaPaquetes();
+
+	public Tiempo solicitarTiempoEstimadoEntregaPaquete(Paquete p);
+
+	public void notificarDestino(Destino d);
 }
