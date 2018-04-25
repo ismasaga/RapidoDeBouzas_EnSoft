161e6552ca93a4920a75d1fc457d2b7157369738
ismasaga <ismael.saborido@outlook.es>
2018-04-25 16:09:37 +0200

    Controlador actualizado

--- src/controlador/Controlador.java
+++ src/controlador/Controlador.java
@@ -2,13 +2,27 @@ package controlador;
 
 import java.util.ArrayList;
 
+import comunicacion_sistema_central.SistemaCentral;
 import principal.*;
 
 public class Controlador implements InterfazControlador {
 
 	private ArrayList<Paquete> listaEntrega;
 	private ArrayList<Paquete> listaDevolucion;
-
+	private SistemaCentral sc = new SistemaCentral();
+	
+	/*private Controlador() {
+		// FACER SINGLETON PARA INSTANCIAR O CONTROLADOR (QUE INSTANCIA LalizacionControl)
+	}*/
+	
+	public Controlador() {
+		
+	}
+	
+/**
+ * Se quedan hay que metelos no diagrama de clases
+ * @return
+ */
 	public ArrayList<Paquete> getListaDevolucion() {
 		return listaDevolucion;
 	}
@@ -21,88 +35,156 @@ public class Controlador implements InterfazControlador {
 		this.listaEntrega = listaEntrega;
 	}
 
+	
+	
+//---------------------------------------------------------------------------
+	
+	
+	
+	/**
+	 * Metodo que redirixe a notificacion de parada que proven do
+	 * subsistema de localizacion e xestion
+	 */
 	@Override
 	public void notificarDetencion(Camion c) {
-		System.out.println("Camion en controlador: ");
-		System.out.println("Posicion camion: (" + c.getLocalizacion().getX() + ", " + c.getLocalizacion().getY() + ")");
+		sc.escribirTiempoAleatorio(c);
 	}
 
+	/**
+	 * Devolve o paquete que concorde coa id se recibe por parametro
+	 * Devolve null en caso de que non exista
+	 */
 	@Override
 	public Paquete solicitarPaquete(String id) {
+		for (Paquete paquete : listaDevolucion) {
+			if (paquete.getId().equals(id)) {
+				return paquete;
+			}
+		}
 		return null;
 	}
 
+	/**
+	 * Engade un paquete na lista de paquetes
+	 * Xestiona internamente se e para devolucion ou para repartir
+	 */
 	@Override
 	public void anadirPaquete(Paquete p) {
-		// TODO Auto-generated method stub
-
+		if (p.getEstado() == EstadoPaquete.DEVOLUCION) {
+			listaDevolucion.add(p);
+		} else if (p.getEstado() == EstadoPaquete.EN_REPARTO) {
+			listaEntrega.add(p);
+		}
 	}
 
+	/**
+	 * Borra un paquete de unha das listas
+	 * Se o borra de ambas sae erro pola consola
+	 */
 	@Override
 	public void borrarPaquete(String id) {
-		// TODO Auto-generated method stub
-
+		int flag = 0;
+		
+		for (Paquete p : listaDevolucion) {
+			if (p.getId().equals(id)) {
+				listaDevolucion.remove(p);
+				System.out.println("Paquete eliminado da lista de paquetes a devolver");
+				flag++;
+			}
+		}
+		for (Paquete p : listaEntrega) {
+			if (p.getId().equals(id)) {
+				listaEntrega.remove(p);
+				System.out.println("Paquete eliminado da lista de paquetes a entregar");
+				flag++;
+			}
+		}
+		if (flag > 1) {
+			System.err.println("ERRO!!! O paquete foi eliminado das dúas listas, comportamento anómalo");
+		}
 	}
 
+	/**
+	 * ********METODO DUPLICADO CON solicitarPaquete(String id)*******
+	 * Devolve o paquete que concorde coa id se recibe por parametro
+	 * Devolve null en caso de que non exista
+	 */
 	@Override
 	public Paquete buscarPaquete(String id) {
-		// TODO Auto-generated method stub
+		for (Paquete paquete : listaDevolucion) {
+			if (paquete.getId().equals(id)) {
+				return paquete;
+			}
+		}
 		return null;
 	}
 
 	@Override
 	public void modificarPaquete(Paquete p) {
-		// TODO Auto-generated method stub
-
+		// Busco polo id e igualo todo menos o id (non se pode modificar)
 	}
 
+	/**
+	 * Redirixe o camion que ven do sistema de localizacion para o sistema central
+	 * que e quen o precisa para escribir
+	 */
 	@Override
 	public void enviarCamion(Camion c) {
-		// TODO Auto-generated method stub
-
+		sc.notificarDetencion(c);
 	}
 
+	/**
+	 * Devolve a lista de paquetes a entregar
+	 */
 	@Override
 	public ArrayList<Paquete> solicitarListaPaquetes() {
-		// TODO Auto-generated method stub
-		return null;
+		return listaEntrega;
 	}
 
 	@Override
 	public Tiempo solicitarTiempoEstimadoEntregaPaquete(Paquete p) {
-		// TODO Auto-generated method stub
+		// Cando faga o singleton vou chamar a localizacion e control (buscarFecha(p.getDestino))
 		return null;
 	}
 
 	@Override
 	public Camion recibirCamion() {
-		// TODO Auto-generated method stub
+		// VER SE FAI FALLA
 		return null;
 	}
 
 	@Override
 	public Paquete confirmarPaquete() {
-		// TODO Auto-generated method stub
+		// VER SE FAI FALLA
 		return null;
 	}
 
+	/**
+	 * Devolve un paquete a devolver
+	 * Realmente sempre devolve o primeiro asi que se non se elimina sempre devolvera o mesmo
+	 * @return
+	 */
 	public Paquete solicitarDevolucionAleatoria() {
-		return null;
+		Paquete p = listaDevolucion.get(0);
+		return p;
 	}
 
 	public ArrayList<Paquete> getListaEntrega() {
+		// VER SE FAI FALLA
 		return null;
 	}
 
 	public ArrayList<Paquete> getListaDevoluciones() {
+		// VER SE FAI FALLA
 		return null;
 	}
 
 	public void modificarEstadoCamion(EstadoCamion estado) {
-
+		// VER SE FAI FALLA
 	}
 
 	public EstadoCamion getEstadoActualCamion() {
+		// VER SE FAI FALLA
 		return null;
 	}
 
