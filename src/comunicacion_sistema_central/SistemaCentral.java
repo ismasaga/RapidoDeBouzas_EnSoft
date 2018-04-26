481ba4ca8d5cd8b989c023f3aa5225e05ad484c2
ismasaga <ismael.saborido@outlook.es>
2018-04-26 17:54:33 +0200

    Reparados varios fallos

--- src/comunicacion_sistema_central/SistemaCentral.java
+++ src/comunicacion_sistema_central/SistemaCentral.java
@@ -1,6 +1,12 @@
 package comunicacion_sistema_central;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 import principal.*;
 
@@ -35,83 +41,117 @@ public  class SistemaCentral implements InterfazComunicacionSistemaCentral {
 		//System.out.println("Detención de camión en destino");
 		//System.out.println("Posicion camion: (" + c.getLocalizacion().getX() + ", " + c.getLocalizacion().getY() + ")");
 		System.out.println("No implementada");
+		return true;
 	}
 	
 	@Override
 	public ArrayList<String> listarParadas(){
 		System.out.println("No implementada");
+		return null;
 	}
 	
 	@Override
 	public void confirmarPaquete(Paquete p) {
-		p.setEstado (ENTREGADO);				//suponiendo que uno de las opciones de estado de paquete sea entregado
+		p.setEstado (EstadoPaquete.ENTREGADO);				//suponiendo que uno de las opciones de estado de paquete sea entregado
 	}
 	
-	private ArrayList<Paquete> leer_archivo_entregas () throws FileNotFoundException, IOException{
+	private ArrayList<Paquete> leer_archivo_entregas () {
 		String archivo = "fichero_entregas.csv";
 		ArrayList <Paquete> array = new ArrayList <Paquete>();
-		
 		String cadena;
-        FileReader f = new FileReader(archivo);
-        BufferedReader b = new BufferedReader(f);
-        while((cadena = b.readLine())!=null) {
-        	
-        	StringTokenizer st = new StringTokenizer(cadena,";");
-        	
-        	String dni = st.nextToken();
-        	String nombre = st.nextToken();
-        	float pos_x = st.nextToken();
-        	float pos_y = st.nextToken();
-        	String tel = st.nextToken();
-        	String id_paq = st.nextToken();
-        	
-        	Destino des = new Destino (pos_x, pos_y);
-        	Cliente cli = new Cliente (dni, nombre, des, tel, null);		// estoy metiendo la firma a null
-        	
-        	Paquete p = new Paquete(id_paq, des, cli, null, null, null, false, null);
-        	
-        	array.add(p);
-        	
-        }
+        FileReader f = null;
         
-        b.close();
+		try {
+			f = new FileReader(archivo);
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+        BufferedReader b = new BufferedReader(f);
+        try {
+			while((cadena = b.readLine())!=null) {
+				
+				StringTokenizer st = new StringTokenizer(cadena,";");
+				
+				String dni = st.nextToken();
+				String nombre = st.nextToken();
+				float pos_x = Float.parseFloat(st.nextToken());
+				float pos_y = Float.parseFloat(st.nextToken());
+				String tel = st.nextToken();
+				String id_paq = st.nextToken();
+				
+				Destino des = new Destino (pos_x, pos_y);
+				Cliente cli = new Cliente (dni, nombre, des, tel, false);		// estoy metiendo la firma a false
+				
+				Paquete p = new Paquete(id_paq, des, cli, null, null, null, false, null);
+				
+				array.add(p);
+				
+			}
+		} catch (NumberFormatException | IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+        
+        try {
+			b.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
         
         return array;
 	}
 	
-	private ArrayList<Paquete> leer_archivo_devoluciones () throws FileNotFoundException, IOException{
+	private ArrayList<Paquete> leer_archivo_devoluciones () {
 		String archivo = "fichero_devoluciones.csv";
 		ArrayList<Paquete> array = new ArrayList<Paquete>();
 		Paquete p = null;
-		
 		String cadena;
-        FileReader f = new FileReader(archivo);
-        BufferedReader b = new BufferedReader(f);
-        while((cadena = b.readLine())!=null) {
-        	
-        	StringTokenizer st = new StringTokenizer(cadena,";");
-        	
-        	String dni = st.nextToken();
-        	String nombre = st.nextToken();
-        	float pos_x = st.nextToken();
-        	float pos_y = st.nextToken();
-        	String tel = st.nextToken();
-        	String id_paq = st.nextToken();
-        	
-        	Destino des = new Destino (pos_x, pos_y);
-        	Cliente cli = new Cliente (dni, nombre, des, tel, null);		// estoy metiendo la firma a null
-        	
-        	p = new Paquete(id_paq, des, cli, null, null, null, false, null);
-        	
-        	array.add(p);
-        }
+        FileReader f = null;
         
-        b.close();
+		try {
+			f = new FileReader(archivo);
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+        BufferedReader b = new BufferedReader(f);
+        try {
+			while((cadena = b.readLine())!=null) {
+				
+				StringTokenizer st = new StringTokenizer(cadena,";");
+				
+				String dni = st.nextToken();
+				String nombre = st.nextToken();
+				float pos_x = Float.parseFloat(st.nextToken());
+				float pos_y = Float.parseFloat(st.nextToken());
+				String tel = st.nextToken();
+				String id_paq = st.nextToken();
+				
+				Destino des = new Destino (pos_x, pos_y);
+				Cliente cli = new Cliente (dni, nombre, des, tel, false);		// estoy metiendo la firma a false
+				
+				p = new Paquete(id_paq, des, cli, null, null, null, false, null);
+				
+				array.add(p);
+			}
+		} catch (NumberFormatException | IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+        
+        try {
+			b.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
         
         return array;
 	}
 
-	private boolean escribir_camion (Camion c) throws IOException {
+	private boolean escribir_camion (Camion c) {
 		try{
 		    String filename= "fichero_escritura.csv";
 		    FileWriter fw = new FileWriter(filename,true); //the true will append the new data
@@ -126,11 +166,11 @@ public  class SistemaCentral implements InterfazComunicacionSistemaCentral {
 		return true;
 	}
 
-	private boolean escribir_paquete (Paquete p) throws IOException {
+	private boolean escribir_paquete (Paquete p) {
 		try{
 		    String filename= "fichero_escritura.csv";
 		    FileWriter fw = new FileWriter(filename,true); //the true will append the new data
-		    fw.write(p.getId() + "; " + p.getCliente().getDni() "\n");
+		    fw.write(p.getId() + "; " + p.getCliente().getDni() + "\n");
 		    fw.close();
 		}
 		catch(IOException ioe) {
